ef5b8199f10656ed74a5907c0e8df1d0e2d827bc
William Horn <wbhorn@alaska.edu>
2018-04-04 12:20:00 -0800

    Add docker install script

---
+++ docker-install.sh
@@ -0,0 +1,9 @@
+sudo apt-get update
+sudo apt-get install apt-transport-https ca-certificates curl software-properties-common -y
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo apt-key fingerprint 0EBFCD88
+
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu zesty stable" -y
+
+sudo apt-get update
+sudo apt-get install docker-ce -y
